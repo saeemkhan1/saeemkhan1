f95dd6c082e0e9dfd323869876f3f2c531bac57a
saeemkhan1 <63438912+saeemkhan1@users.noreply.github.com>
2022-08-05 23:33:32 +1000

    Update PythonBasics.py

--- PythonBasics.py
+++ PythonBasics.py
@@ -324,3 +324,63 @@ print(b)
 # Cannot add List or Dictionary in a Set
 c = set()
 c.add({"Name": "Python"})
+
+
+
+# Write a program to write a dictionary of words with meanings,
+# user can select words and will get a reply in return
+
+dictionary = {"Admire": "Regard with respect",
+              "Great": "Very Well",
+              "Awesome": "Fantastic",
+              "Mighty": "Very Strong"
+           }
+print("Enter any words from below:\n", dictionary.keys())
+z = input("Enter the Word:")
+print("Meaning of your word is: ", dictionary.get(z))
+
+# Write a program to input 8 numbers from user, and display only the unique numbers
+a = set()
+b = int(input("Enter your element: "))
+c = int(input("Enter your element: "))
+d = int(input("Enter your element: "))
+e = int(input("Enter your element: "))
+f = int(input("Enter your element: "))
+g = int(input("Enter your element: "))
+h = int(input("Enter your element: "))
+i = int(input("Enter your element: "))
+
+a.add(b)
+a.add(c)
+a.add(d)
+a.add(e)
+a.add(f)
+a.add(g)
+a.add(h)
+a.add(i)
+
+print(a)
+
+
+# Write a program to input 8 numbers from user, and display only the unique numbers
+setB = set()
+x = int(input("Enter your element: "))
+y = input("Enter your element: ")
+
+setB.add(x)
+setB.add(y)
+print(setB)
+
+# What will be the length of the following set
+
+s = set()
+s.add(20)
+s.add(20.0)
+s.add("20")
+print(len(s))
+
+# What is the type of this set
+v = {}
+print(type(v))
+
+
