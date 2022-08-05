8ec8043d5b737cc7721568e7c13e8968958c7c0b
saeemkhan1 <63438912+saeemkhan1@users.noreply.github.com>
2022-08-05 15:26:48 +1000

    Update PythonBasics.py

--- PythonBasics.py
+++ PythonBasics.py
@@ -284,21 +284,43 @@ print(myDictionary.get("medium2"))     # Will return None as "medium2" is not pr
 print(myDictionary["medium2"])         # Will throw an error as "medium2" is not present in the Dictionary
 
 
+#########
+# Sets
+#########
+
+setA = {1, 2, 3, 5, 5, 5}   # Set will not have repetitive elements inside
+print(type(setA))
+print(setA)
+
+# This syntax will create an empty dictionary and not an empty set
+a = {}
+print(type(a))
+
+# An empty set can be created using the below syntax
+
+b = set()
+print(type(b))
+
+# Adding Values in an empty set using .add method
+b.add("StringA")
+b.add(4)
+b.add(5)
+b.add(5)            # Can not add duplicate values in Set
+b.add(23)
+b.add((2, 4, 7))    # Can add Tuple in a Set
+
+print(b)
+print(len(b))       # Prints the Length of the Set
 
 
+b.remove(23)        # Removes 23 from Set b
+print(b)
+print(len(b))
+
+b.pop()             # Removes random element from the set b
+print(b)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Cannot add List or Dictionary in a Set
+c = set()
+c.add({"Name": "Python"})
